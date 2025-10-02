b212c202abd01058ef84578b7961e0bc27165013
OhmuraYuta <ohmura.y.24kdgn@gmail.com>
2025-10-02 21:52:04 +0900

    telescopeを読み込まない

--- app/bootstrap/providers.php
+++ app/bootstrap/providers.php
@@ -2,5 +2,5 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    App\Providers\TelescopeServiceProvider::class,
+    // App\Providers\TelescopeServiceProvider::class, 本番で使わないのに読み込まれてエラーが出た
 ];
